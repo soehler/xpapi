67f49bbc3ab0b67b9f1c51575a1784ac5b164efc
Sergio Oehler <soehler@truesys.com>
2021-05-16 20:39:32 -0300

    added cors support

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require("express");
+var cors = require('cors')
 var app = express();
 
-app.get("/users", (req, res, next) => {
-    res.json(["Joao","Maria","Jose","Renata","Augusto"]);
+const whitelist = ['http://localhost', 'http://hspw.org']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
+app.get("/users",cors(), (req, res, next) => {
+    res.json({"users":["Joao","Maria","Jose","Renata","Augusto"]});
    });
 
-app.listen(3000, () => {
- console.log("XPAPI Server running on port 3000");
+app.listen(3001, () => {
+ console.log("XPAPI Server running on port 3001");
 });
 
